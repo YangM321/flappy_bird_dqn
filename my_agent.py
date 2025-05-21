a813a0478621a1308b1eef11a0e1dcef2c67ea24
YangM321 <mingyang.zhang@student.adelaide.edu.au>
2025-05-21 21:29:24 +0930

    level1 done

--- my_agent.py
+++ my_agent.py
@@ -3,9 +3,11 @@ import pygame
 from pytorch_mlp import MLPRegression
 import argparse
 from console import FlappyBirdEnv
+from collections import deque
+import random
 
-STUDENT_ID = 'a1234567'
-DEGREE = 'UG'  # or 'PG'
+STUDENT_ID = 'a1785401'
+DEGREE = 'UG'
 
 
 class MyAgent:
@@ -18,21 +20,53 @@ class MyAgent:
             self.mode = mode
 
         # modify these
-        self.storage = ...  # a data structure of your choice (D in the Algorithm 2)
-        # A neural network MLP model which can be used as Q
-        self.network = MLPRegression(input_dim=..., output_dim=..., learning_rate=...)
+        self.storage = deque(maxlen=10000)
+        self.network = MLPRegression(input_dim=8, output_dim=3, learning_rate=0.001)
         # network2 has identical structure to network1, network2 is the Q_f
-        self.network2 = MLPRegression(input_dim=..., output_dim=..., learning_rate=...)
+        self.network2 = MLPRegression(input_dim=8, output_dim=3, learning_rate=0.001)
         # initialise Q_f's parameter by Q's, here is an example
         MyAgent.update_network_model(net_to_update=self.network2, net_as_source=self.network)
 
-        self.epsilon = ...  # probability ε in Algorithm 2
-        self.n = ...  # the number of samples you'd want to draw from the storage each time
-        self.discount_factor = ...  # γ in Algorithm 2
+        self.epsilon = 1  # probability ε in Algorithm 2
+        self.n = 64  # the number of samples you'd want to draw from the storage each time
+        self.discount_factor = 0.99  # γ in Algorithm 2
 
         # do not modify this
         if load_model_path:
             self.load_model(load_model_path)
+
+    def extract_features(self, state: dict) -> np.ndarray:
+
+        bird_x = state['bird_x']
+        bird_y = state['bird_y']
+        bird_velocity = state['bird_velocity']
+        screen_width = state['screen_width']
+        screen_height = state['screen_height']
+
+        if state['pipes']:
+            pipe = state['pipes'][0]
+            pipe_x = pipe.get('x', 400)
+            pipe_top_y = pipe.get('top', 0)
+            pipe_bottom_y = pipe.get('bottom', 600)
+        else:
+            pipe_x = 400
+            pipe_top_y = 0
+            pipe_bottom_y = 600
+
+        features = np.array([
+            bird_y,
+            bird_velocity,
+            screen_width,
+            screen_height,
+            pipe_x - bird_x,
+            pipe_top_y,
+            pipe_bottom_y,
+            pipe_bottom_y - bird_y,
+        ], dtype = np.float32)
+
+        return features
+
+
 
     def choose_action(self, state: dict, action_table: dict) -> int:
         """
@@ -44,7 +78,16 @@ class MyAgent:
             action: the action code as specified by the action_table
         """
         # following pseudocode to implement this function
-        a_t = ...
+
+        features = self.extract_features(state).reshape(1,-1)
+        if self.mode == 'train' and np.random.rand() < self.epsilon:
+            a_t = np.random.choice(list(action_table.values())[:2])
+        else:
+            q_values = self.network.predict(features)
+            a_t = int(np.argmax(q_values))
+
+        self.prev_state = self.extract_features(state)
+        self.prev_action = a_t
 
         return a_t
 
@@ -58,6 +101,40 @@ class MyAgent:
             None
         """
         # following pseudocode to implement this function
+
+        new_state_vec = self.extract_features(state)
+        done = state['done']
+        reward = 1
+        if done:
+            reward = -100
+
+        self.storage.append((self.prev_state, self.prev_action, reward, new_state_vec, done))
+
+        if self.mode == 'train' and len(self.storage) >= self.n:
+            batch = random.sample(self.storage, self.n)
+            states, actions, rewards, next_states, dones = zip(*batch)
+
+            states = np.array(states)
+            actions = np.array(actions)
+            rewards = np.array(rewards)
+            next_states = np.array(next_states)
+            dones = np.array(dones)
+
+            next_q_values = self.network2.predict(next_states)
+            max_next_q = np.max(next_q_values, axis = 1)
+            targets = rewards + self.discount_factor * max_next_q * (1-dones) 
+
+            q_values = self.network.predict(states)
+            for i in range(self.n):
+                q_values[i][actions[i]] = targets[i]
+            W = np.ones_like(q_values)
+            self.network.fit_step(states, q_values, W)
+            
+
+
+
+
+
 
     def save_model(self, path: str = 'my_model.ckpt'):
         """
@@ -120,9 +197,15 @@ if __name__ == '__main__':
 
         # you'd want to clear the memory after one or a few episodes
         ...
+        if episode % 20 == 0:
+            agent.storage.clear()
 
         # you'd want to update the fixed Q-target network (Q_f) with Q's model parameter after one or a few episodes
         ...
+        if episode % 10 == 0:
+            MyAgent.update_network_model(agent.network2, agent.network)
+        
+        agent.epsilon = max(0.1, agent.epsilon * 0.99)
 
     # the below resembles how we evaluate your agent
     env2 = FlappyBirdEnv(config_file_path='config.yml', show_screen=False, level=args.level)
